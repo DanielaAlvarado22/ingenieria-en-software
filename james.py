0eb9bf67907dd0702e69ca629115d02e1cc1baa6
Daniel López <hyperdaniel579exe@gmail.com>
2022-08-30 19:48:45 -0500

    Updated

--- james.py
+++ james.py
@@ -1,7 +1,9 @@
 import re
 from xml import dom
 import datetime
-horarios = "Sun 10:00-20:00\nFri 05:00-10:00\nFri 16:30-23:50\nSat 10:00-24:00\nSun 01:00-04:00\nSat 02:00-06:00\nTue 03:30-18:15\nTue 19:00-20:00\nWed 04:25-15:14\nWed 15:14-22:40\nThu 00:00-23:59\nMon 05:00-13:00\nMon 15:00-21:00"
+
+hrsTest1 = 'Sun 10:00-20:00\nFri 05:00-10:00\nFri 16:30-23:50\nSat 10:00-24:00\nSun 01:00-04:00\nSat 02:00-06:00\nTue 03:30-18:15\nTue 19:00-20:00\nWed 04:25-15:14\nWed 15:14-22:40\nThu 00:00-23:59\nMon 05:00-13:00\nMon 15:00-21:00'
+hrsTest2 = 'Mon 01:00-22:00\nTue 04:00-18:00\nWed 01:00-23:00\nThu 08:00-20:00\nFri 10:00-22:00\nSat 02:00-22:00\nSun 06:00-22:00'
 
 def primera(e):
     return datetime.timedelta(hours=int(e[4]+e[5]),minutes=int(e[7]+e[8]))
@@ -9,7 +11,7 @@ def segunda(e):
     return datetime.timedelta(hours=int(e[10]+e[11]),minutes=int(e[13]+e[14]))
     
 
-separados = re.split(r'\n', horarios)
+separados = re.split(r'\n', hrsTest2)
 lunes = ['Mon 00:00-00:00']
 martes = []
 miercoles = []
